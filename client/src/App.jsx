50fcdfe47d44817f949e80a1411b2f600cfdb637
AbhijitKhyade <abhijitkhyade@gmail.com>
2024-04-17 00:43:17 +0530

    Add Material Tailwind React library and update dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Button } from "@material-tailwind/react";
 
 export default function App() {
   return (
-    <div className='text-red-700 text-3xl font-semibold'>Welcome</div>
+    <div className='text-red-700 text-3xl font-semibold'>
+      Welcome
+    </div>
   )
 }
