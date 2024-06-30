f04feaa89ed562a6af0dbcd77a61e19a6780f420
AbhijitKhyade <abhijitkhyade@gmail.com>
2024-06-30 10:25:55 +0530

    Update the BASE_URL for production

--- client/src/api.js
+++ client/src/api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:8080/api";
-// export const BASE_URL= "";
+// export const BASE_URL = "http://localhost:8080/api";
+export const BASE_URL= "https://district-scheme-tracker.onrender.com/api";
